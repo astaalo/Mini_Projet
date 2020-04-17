31ae3b11eb9b97c812b5886b5c76c5381220d209
astaa <62703786+astaalo@users.noreply.github.com>
2020-04-17 00:45:03 +0000

    Update and rename Mini_Projet/Accueil.php to  Mini_Projet/Accueil.php

--- Mini_Projet/Accueil.php
+++ Mini_Projet/Accueil.php
@@ -1,4 +1,4 @@
-<!DOCTYPE HTML>
+<!DOCTYPE HTML><!DOCTYPE HTML>
 <html>
 <head>
  <meta charset="utf-8">
@@ -8,6 +8,78 @@
      <body> 
         <div class="blog">  
             <div id="banniere">
+                <div class="logo">
+                    <img src="img/logo-QuizzSA.png">
+                </div>
+                    <p> Le plaisir de jouer</p>
+            </div>
+            <div id="milieu">
+                <div id=conteneur>
+                    <div id="log"><h4> CREER ET PARAMETREZ VOS QUIZZ </h4>
+                        <!--<a href="deconnexion.php"><button type="submit" name="deconnexion">Deconnexion</button></a></div>-->
+                        <a href="PageConnexion.php"><button type="submit" name="deconnexion">Deconnection</button></a></div>
+                        <div id="blog1">
+                            <div id="avatar">
+                                <a href="#"><img src="img/img5.jpg" alt=""></a>
+                                    <div id="menu">
+                                        <ul>
+                                            <li><a href="#" target="_top">Liste Questions</a></li>
+                                            <div class="ic-liste"><img src="img/ic-liste.png"></div><br>
+                                            <li><a href="CreerCompteAdmin.php" target="_top">Creer Admin</a></li>
+                                            <div class="ic-ajout"><img src="img/ic-ajout.png"></div><br> 
+                                            <li><a href="#" target="_top">Liste Joueurs</a></li>
+                                            <div class="ic-liste"><img src="img/ic-liste.png"></div><br>
+                                            <li><a href="#" target="_top">Creer Questions</a></li>
+                                            <div class="ic-ajout"><img src="img/ic-ajout.png"></div>
+                                        </ul>
+                                    </div>
+                            </div>
+                            <div class="droite">
+                                <div id="para"><h5> LISTE DES JOUEURS PAR SCORE </h5></div>
+                                    <div class="blogL">
+                                        <table>
+                                        <thead> 
+                                            <tr>
+                                                <td><strong>Prenom</strong>
+                                                    <p>Asta</p>
+                                                    <p> Silvie</p>
+                                                    <p>Ibrahima</p>
+                                                    <p>Maryam</p>
+                                                    <p>Ahmeth</p>
+                                                    <p>Oumy</p>
+                                                    <p>Aly</p>
+                                                </td>
+                                                <td><strong>Nom</strong>
+                                                    <p>Fall</p>
+                                                    <p>Diouf</p>
+                                                    <p>Gueye</p>
+                                                    <p>Sylla</p>
+                                                    <p>Faye</p>
+                                                    <p>Niang</p>
+                                                    <p>Dieye</p>
+                                                </td>
+                                                <td><strong>Score(pts)</strong>
+                                                    <p>120 pts</p> 
+                                                    <p>850 pts</p>
+                                                    <p>154 pts</p>
+                                                    <p>40 pts</p>
+                                                    <p>370 pts</p>
+                                                    <p>403 pts</p>
+                                                    <p>190 pts</p>
+                                                </td>
+                                            </tr>
+                                        </thead> 
+                                        </table>
+                                    </div><br>
+                                        <button type="submit" name="suivant">Suivant</button>
+                            </div>
+                        </div>
+                     </div>
+                </div>
+            </div>
+        </div>    
+    </body>
+    </html>
             <div class="logo">
                 <img src="img/logo-QuizzSA.png">
             </div>
